5d46733057a54cc9769089c8f767e5fed45f9757
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-23 16:16:24 -0300

    Finish public button on show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'public_recipes', to: 'recipes#public_index'
+  post 'recipes/:id/update_public', to: 'recipes#update_public'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
