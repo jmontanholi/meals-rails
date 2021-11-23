35704ecd8886acff57548c5c59c1bfc5ea258a8a
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-23 09:48:59 -0600

    Scaffold initial data models

---
+++ spec/views/recipe_foods/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "recipe_foods/index", type: :view do
+  before(:each) do
+    assign(:recipe_foods, [
+      RecipeFood.create!(
+        quantity: "9.99",
+        recipe: nil,
+        food: nil
+      ),
+      RecipeFood.create!(
+        quantity: "9.99",
+        recipe: nil,
+        food: nil
+      )
+    ])
+  end
+
+  it "renders a list of recipe_foods" do
+    render
+    assert_select "tr>td", text: "9.99".to_s, count: 2
+    assert_select "tr>td", text: nil.to_s, count: 2
+    assert_select "tr>td", text: nil.to_s, count: 2
+  end
+end
