9c586493c47f62f22f5bb8e666f0a9012a620e84
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-23 10:13:00 -0600

    Add routes for public recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'recipes#index'
   resources :recipe_foods
   resources :foods
   resources :recipes
   devise_for :users
-  
+
+  get 'public_recipes', to: 'recipes#public_index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
