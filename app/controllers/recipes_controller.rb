5fc29a5cfe25fb34f9da981fe43aaccc85fe7725
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-25 11:58:18 -0600

    Fix path for recipes in form and in create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -37,7 +37,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
+        format.html { redirect_to user_recipes_path, notice: 'Recipe was successfully created.' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
